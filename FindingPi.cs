3736aaf4ada4b0f90f332bb1a67adfc5f9222a46
PetarGerzilov <43782433+PetarGerzilov@users.noreply.github.com>
2018-12-11 22:25:05 +0200

    Add files via upload

---
+++ FindingPi.cs
@@ -0,0 +1,29 @@
+﻿using System;
+
+namespace FindingPi
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            double a=double.Parse(Console.ReadLine());
+            int counter = 1;
+            double sum = 0;
+            for (int i = 1; i < 10000; i+=2)
+            {
+                if (counter % 2 == 0)
+                {
+                    sum = sum + (a / i);
+                    counter++;
+                }
+                else
+                {
+                    sum = sum - (a / i);
+                    counter++;
+                }
+            }
+            double p = 4 * sum;
+            Console.WriteLine(p);
+        }
+    }
+}
